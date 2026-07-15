66d8c0e333f846d4ae0690835ee3a738f546f790
Kesav Arun <akesavarun@gmail.com>
2026-07-15 20:27:39 +0530

    home page faq and footer remaining

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import HomeLap from './HomeLap'
+
+function App() {
+  return(
+    <>
+      <HomeLap/>
+    </>
+  )
+}
+
+export default App
---
+++ src/HomeLap.jsx
@@ -0,0 +1,84 @@
+import './HomeLap.css'
+
+function HomeLap(){
+
+  const reasons = [
+    {
+      id:1,
+      title:"Battery 🔋",
+      text:"Battery degradation over time reduces the available driving range and overall performance."
+    },
+    {
+      id:2,
+      title:"Weather ☁️🌡️",
+      text:"Hot and cold temperatures affect battery efficiency, reducing the distance an EV can travel."
+    },
+    {
+      id:3,
+      title:"Terrain ⛰️  ",
+      text:"Hills, rough roads, and traffic increase energy consumption during a trip."
+    },
+    {
+      id:4,
+      title:"Charging Network ⚡",
+      text:"Knowing nearby charging stations helps plan longer journeys with confidence."
+    },
+    {
+      id:5,
+      title:"Driving Style 🚗",
+      text:"Frequent acceleration and high speeds consume more battery power than smooth driving."
+    },
+    {
+      id:6,
+      title:"Vehicle Load 👪",
+      text:"Extra passengers and luggage increase vehicle weight, reducing driving range."
+    }
+  ]
+
+  const mreasons = reasons.map((reason)=>({
+    id:reason.id,
+    title:reason.title,
+    text:reason.text
+  }))
+
+  return(
+    <>
+      <div className="header">
+        <div className="logo-heading">
+          <img src="/logo-car.png" alt="" className="logo-header" />
+          <p className="heading">EV TRIP ANALYZER</p>
+        </div>
+
+        <p className="home-link">Home</p>
+        <p className="start-link">Start</p>
+        <button className='analyze-btn' >Analyze</button>
+
+      </div>
+
+      <div className="hero-sec">
+        <div className="hero-text">
+          <p className="text-heading">Powering Smarter <br /> EV Decisions</p>
+          <p className="text-below">Analyze electric vehicles by battery capacity, driving range, charging performance, and efficiency through an intuitive, data-driven dashboard.</p>
+          
+        </div>
+        <img src="/logo-hero.jpg" alt="" className="logo-hero" />
+      </div>
+      <p className="why-q">Why Accurate EV Range Estimation Matters ?</p>
+      <img src="/arrow-down.png" alt="" className="arrow-down" />
+      <div className="plan-smarter">
+        <p className="plan-text">Plan smarter journeys with confidence by understanding the factors that influence your vehicle's real-world range.</p>
+      </div>
+      <div className="details-container">
+        {mreasons.map((reason)=>(
+        <div className="details-box" key={reason.id}>
+        <p className="title">{reason.title}</p>
+        <p className="text">{reason.text}</p>
+      </div>
+      ))}
+      </div>
+      
+    </>
+  )
+}
+
+export default HomeLap
